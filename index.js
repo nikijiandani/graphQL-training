e44c8af973ad45244eeb57c922b6bd487cbfe575
Nikita Jiandani <niki.jiandani@gmail.com>
2019-11-20 23:31:46 -0500

    Add simple graphql server

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-import express from 'express'
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema from './schema';
 
-const app = express()
+const app = express();
 
 app.get('/', (req, res) => {
-  res.send('GraphQL is amazing!!!')
-})
+  res.send('GraphQL is amazing!!!');
+});
 
-app.listen(8080, () => console.log('Running server on port localhost:8080/graphql'))
+const root = { hello: () => "Hi I'm Niki" };
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
+
+app.listen(8080, () =>
+  console.log('Running server on port localhost:8080/graphql')
+);
