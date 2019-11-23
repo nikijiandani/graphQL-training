b1de4749be16d08d4b0851dbf2684a37683ffce7
Nikita Jiandani <niki.jiandani@gmail.com>
2019-11-23 18:06:42 -0500

    Add resolvers

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import schema from './schema';
+import resolvers from './resolvers'
 
 const app = express();
 
@@ -8,38 +9,7 @@ app.get('/', (req, res) => {
   res.send('GraphQL is amazing!!!');
 });
 
-class Friend {
-  constructor(id, { firstName, lastName, gender, language, email }) {
-    this.id = id;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.gender = gender;
-    this.language = language;
-    this.email = email;
-  }
-}
-
-const friendDatabase = {};
-
-const root = {
-  friend: () => {
-    return {
-      id: 2346985547409,
-      firstName: 'Niki',
-      lastName: 'Jiandani',
-      gender: 'does it matter?',
-      language: 'Anglais',
-      emails: [{ email: 'me@me.com' }, { email: 'another@me.com' }]
-    };
-  },
-  createFriend: ({ input }) => {
-    let id = require('crypto')
-      .randomBytes(10)
-      .toString('hex');
-    friendDatabase[id] = input;
-    return new Friend(id, input);
-  }
-};
+const root = resolvers;
 
 app.use(
   '/graphql',
---
+++ resolvers.js
@@ -0,0 +1,27 @@
+class Friend {
+  constructor(id, { firstName, lastName, gender, language, email }) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.gender = gender;
+    this.language = language;
+    this.email = email;
+  }
+}
+
+const friendDatabase = {};
+
+const resolvers = {
+  getFriend: ({id}) => {
+    return new Friend(id, friendDatabase[id]);
+  },
+  createFriend: ({ input }) => {
+    let id = require('crypto')
+      .randomBytes(10)
+      .toString('hex');
+    friendDatabase[id] = input;
+    return new Friend(id, input);
+  }
+};
+
+export default resolvers;
